cf20893def752bf2df24280e9ba2557d94e7820b
OlegL1990 <olegl1990@gmail.com>
2018-03-14 09:11:32 +0300

    2.5.2

--- src/main/java/ru/bellintegrator/eas/Application.java
+++ src/main/java/ru/bellintegrator/eas/Application.java
@@ -6,9 +6,10 @@ import org.springframework.context.annotation.ComponentScan;
 import ru.bellintegrator.eas.dao.impl.OfficeDAOImpl;
 import ru.bellintegrator.eas.dao.impl.OrgDAOImpl;
 import ru.bellintegrator.eas.dao.impl.EmployeeDAOImpl;
+import ru.bellintegrator.eas.service.impl.EmployeeServiceImpl;
 
 @SpringBootApplication
-@ComponentScan(basePackageClasses = {OfficeDAOImpl.class, OrgDAOImpl.class, EmployeeDAOImpl.class})
+@ComponentScan(basePackageClasses = {OfficeDAOImpl.class, OrgDAOImpl.class, EmployeeDAOImpl.class, EmployeeServiceImpl.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
